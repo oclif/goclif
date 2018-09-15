c105826240457289d2354daf40e0fa426d12303d
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-15 06:38:42 -0700

    map stdout/stderr

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const net = require('net')
 const fs = require('fs')
 const heroku = require('heroku')
-console.dir(heroku)
 
 try {
   // remove existing socket if exists
   fs.unlinkSync('/tmp/foo.sock')
 } catch {}
+
+const stdout = msg => stdout.write(msg)
+stdout.write = process.stdout.write.bind(process.stdout)
+
+const stderr = msg => stderr.write(msg)
+stderr.write = process.stderr.write.bind(process.stderr)
 
 net
   .createServer()
@@ -17,9 +22,29 @@ net
     console.log('server listening')
     socket.setEncoding('utf8')
     socket.on('data', data => {
-      heroku.run(['version'])
       console.log(`server received: ${data}`)
-      socket.write(data.toUpperCase())
-      socket.end()
+      const send = msg => {
+        stdout(`server sent: ${msg}`)
+        socket.write(msg)
+      }
+      process.stdout.write = d => {
+        send(d)
+      }
+      process.stderr.write = d => {
+        send(d)
+      }
+      heroku.run([data])
+      .then(() => {
+        socket.write(data.toUpperCase())
+        socket.end()
+      })
+      .catch(err => {
+        if (err.code === 'EEXIT') {
+          send(`EEXIT: ${err.oclif.exit}`)
+          socket.end()
+        } else {
+          console.error(err)
+        }
+      })
     })
   })
